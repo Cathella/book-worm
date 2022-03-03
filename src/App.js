8ab82b0d2bf2c14cbc32a682b81ad7f71110e262
Cathella <ncathie214@gmail.com>
2022-03-03 19:00:23 +0300

    Add app routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Discussions from './pages/Discussions';
+import Lessons from './pages/Lessons';
+import Quizzes from './pages/Quizzes';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      Hello App.
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/quizzes" element={<Quizzes />} />
+        <Route exact path="/lessons" element={<Lessons />} />
+        <Route exact path="/discussions" element={<Discussions />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Quizzes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Quizzes() {
+  return (
+    <div>Quizzes</div>
+  );
+}
