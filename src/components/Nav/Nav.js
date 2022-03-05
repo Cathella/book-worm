cb475107c1be04b30e3593f68aeb205dc7c89603
Cathella <ncathie214@gmail.com>
2022-03-05 03:29:23 +0300

    Add logo, style Nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from './logo.svg';
 import './Nav.css';
 
 export default function Nav() {
   return (
     <header>
       <nav>
-        <Link to="/">Home</Link>
+        <div className="logo">
+          <img src={logo} alt="HomeSchool" />
+        </div>
         <Link to="/dashboard">Dashboard</Link>
         <Link to="/lessons">Lessons</Link>
         <Link to="/quizzes">Quizzes</Link>
--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -1,33 +1,37 @@
 import React from 'react';
+import './Auth.css';
 
 export default function Register() {
   return (
     <form>
       <h3>Create new Account</h3>
-      <div>
-        I am a
+      <div className="who-field">
         <span>
           <input type="radio" id="student" name="student" value="Student" checked />
-          Student
+          Student Account
         </span>
         <span>
           <input type="radio" id="teacher" name="teacher" value="Teacher" />
-          Teacher
+          Teacher Account
         </span>
       </div>
-      <input type="text" name="first_name" placeholder="First Name" />
-      <input type="text" name="last_name" placeholder="Last Name" />
+      <div className="fullName">
+        <input type="text" name="first_name" placeholder="First Name" />
+        <input type="text" name="last_name" placeholder="Last Name" />
+      </div>
       <input type="email" name="email" placeholder="Email" />
-      <input list="grades" name="grade" id="grade" placeholder="Grade" />
-      <datalist id="grades">
-        <option>Primary One</option>
-        <option>Primary Two</option>
-        <option>Primary Three</option>
-        <option>Primary Four</option>
-        <option>Primary Five</option>
-        <option>Primary Six</option>
-        <option>Primary Seven</option>
-      </datalist>
+      <div>
+        <input list="grades" name="grade" id="grade" placeholder="Grade" />
+        <datalist id="grades">
+          <option>Primary One</option>
+          <option>Primary Two</option>
+          <option>Primary Three</option>
+          <option>Primary Four</option>
+          <option>Primary Five</option>
+          <option>Primary Six</option>
+          <option>Primary Seven</option>
+        </datalist>
+      </div>
       <input type="password" name="password" placeholder="Password" />
       <button type="submit">Register</button>
     </form>
