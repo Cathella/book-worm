d95e788bf2193a71deadb78332e989d0b3e68ea4
Cathella <ncathie214@gmail.com>
2022-03-10 12:32:11 +0300

    Style auth pages

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
 
 export default function Login() {
   return (
-    <form>
-      <h3>Log into Account</h3>
-      <input type="email" name="email" placeholder="Email" />
-      <input type="password" name="password" placeholder="Password" />
-      <button type="submit">Login</button>
-    </form>
+    <div className="form-container">
+      <div className="row">
+        <section className="col-md-6">
+          Hello
+        </section>
+        <section className="col-md-4 mx-auto">
+          <form>
+            <h3 className="fs-2 mb-5 fw-bold">Sign into your account</h3>
+            <input type="email" name="email" placeholder="Your Email" />
+            <input type="password" name="password" placeholder="Enter your password" />
+            <div className="text-center mb-3">
+              <Link to="/" className="py-1 px-3 bg-light-blue text-decoration-none small-btn text-dark">I forgot my password</Link>
+            </div>
+            <button type="submit" className="fw-bold">Signin</button>
+            <div className="text-center text-muted py-4">Do not have an account?</div>
+            <div className="text-center">
+              <Link to="/register" className="bg-light-blue d-block text-decoration-none big-btn text-dark">
+                <span className="me-4">Register an Account at HomeSchool</span>
+                <FaArrowRight />
+              </Link>
+            </div>
+          </form>
+        </section>
+      </div>
+    </div>
   );
 }
--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -1,39 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
 import './Auth.css';
 
 export default function Register() {
   return (
-    <form>
-      <h3>Create new Account</h3>
-      <div className="who-field">
-        <span>
-          <input type="radio" id="student" name="student" value="Student" checked />
-          Student Account
-        </span>
-        <span>
-          <input type="radio" id="teacher" name="teacher" value="Teacher" />
-          Teacher Account
-        </span>
+    <div className="form-container">
+      <div className="row">
+        <section className="col-md-6">
+          Hello
+        </section>
+        <section className="col-md-5 mx-auto">
+          <form>
+            <h3 className="fs-3 fw-bold mb-3">Signup at HomeSchool</h3>
+            <div className="who-field">
+              <span>
+                <input type="radio" id="student" name="student" value="Student" checked />
+                Student Account
+              </span>
+              <span>
+                <input type="radio" id="teacher" name="teacher" value="Teacher" />
+                Teacher Account
+              </span>
+            </div>
+            <div className="fullName">
+              <input type="text" name="first_name" placeholder="First Name" />
+              <input type="text" name="last_name" placeholder="Last Name" />
+            </div>
+            <input type="email" name="email" placeholder="Email" />
+            <div>
+              <input list="grades" name="grade" id="grade" placeholder="Grade" />
+              <datalist id="grades">
+                <option>Primary One</option>
+                <option>Primary Two</option>
+                <option>Primary Three</option>
+                <option>Primary Four</option>
+                <option>Primary Five</option>
+                <option>Primary Six</option>
+                <option>Primary Seven</option>
+              </datalist>
+            </div>
+            <input type="password" name="password" placeholder="Password" />
+            <button type="submit" className="fw-bold">Signup</button>
+            <div className="text-center text-muted py-4">Already have an account?</div>
+            <div className="text-center">
+              <Link to="/login" className="bg-light-blue d-block text-decoration-none big-btn text-dark">
+                <span className="me-4">Access your account here</span>
+                <FaArrowRight />
+              </Link>
+            </div>
+          </form>
+        </section>
       </div>
-      <div className="fullName">
-        <input type="text" name="first_name" placeholder="First Name" />
-        <input type="text" name="last_name" placeholder="Last Name" />
-      </div>
-      <input type="email" name="email" placeholder="Email" />
-      <div>
-        <input list="grades" name="grade" id="grade" placeholder="Grade" />
-        <datalist id="grades">
-          <option>Primary One</option>
-          <option>Primary Two</option>
-          <option>Primary Three</option>
-          <option>Primary Four</option>
-          <option>Primary Five</option>
-          <option>Primary Six</option>
-          <option>Primary Seven</option>
-        </datalist>
-      </div>
-      <input type="password" name="password" placeholder="Password" />
-      <button type="submit">Register</button>
-    </form>
+    </div>
   );
 }
